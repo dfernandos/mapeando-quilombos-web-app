eff824b73b14a3efd9ee8f20f9eadc8fb0f4d1a7
Daniel Oliveira <danieldfs18@hotmail.com>
2023-07-26 20:31:12 -0300

    Add geolocalization

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   MapContainer,
   TileLayer,
-  Polygon
+  Polygon,
+  Marker,
+  Popup
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { statesData } from './data';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-
-const center = [-30.050890, -51.218222];
 
 function Map() {
+  const navigate = useNavigate();
+  const [center, setCenter] = useState([-30.050890, -51.218222]);
+  const [currentLocation, setCurrentLocation] = useState(null);
 
-
-    const navigate = useNavigate();
-  
-    function getTerritory(territoryId) {
-      console.log(`Ícone clicado para o território com ID: ${territoryId}`);
-      navigate(`/territorio/${territoryId}`, { replace: true }); // Navega para a página de edição com o territoryId como parâmetro de rota
+  useEffect(() => {
+    // Verifica se o navegador suporta a API de geolocalização
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Obtém as coordenadas da geolocalização
+          const { latitude, longitude } = position.coords;
+          // Atualiza o centro do mapa com as coordenadas da geolocalização
+          setCenter([latitude, longitude]);
+          // Define a localização atual para exibir o marcador
+          setCurrentLocation([latitude, longitude]);
+        },
+        (error) => {
+          console.error('Erro ao obter a geolocalização:', error);
+        }
+      );
+    } else {
+      console.error('Geolocalização não suportada pelo navegador.');
     }
+  }, []);
+
+  function getTerritory(territoryId) {
+    console.log(`Ícone clicado para o território com ID: ${territoryId}`);
+    navigate(`/territorio/${territoryId}`, { replace: true }); // Navega para a página de edição com o territoryId como parâmetro de rota
+  }
+
   return (
     <MapContainer
       center={center}
@@ -27,7 +51,7 @@ function Map() {
       style={{ width: '100vw', height: '100vh' }}
     >
       <TileLayer
-        url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=10GyEcePLHFPQHAXn11F	"
+        url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=10GyEcePLHFPQHAXn11F"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
       {
@@ -35,6 +59,7 @@ function Map() {
           const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
 
           return (<Polygon
+            key={state.id}
             pathOptions={{
               fillColor: '#FD8D3C',
               fillOpacity: 0.7,
@@ -54,7 +79,7 @@ function Map() {
                   weight: 2,
                   opacity: 1,
                   color: "white",
-                })
+                });
               },
               mouseout: (e) => {
                 const layer = e.target;
@@ -68,9 +93,18 @@ function Map() {
               },
               click: () => getTerritory(state.id), // Chama getTerritory apenas no clique do polígono
             }}
-          />)
+          />);
         })
       }
+
+      {/* Adiciona o marcador na localização atual */}
+      {currentLocation && (
+        <Marker position={currentLocation} icon={<FontAwesomeIcon icon={faLocationDot} />}>
+        <Popup>
+            <p>Você está aqui, tioooo!</p>
+          </Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 }
