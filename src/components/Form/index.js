abecb0e171cc2cb19afb6902a943c6b210decab3
Daniel Oliveira <danieldfs18@hotmail.com>
2023-07-27 17:30:15 -0300

    remove unused import

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 import { useNavigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import api from '../../Api';
 
