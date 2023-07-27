c67a3bd50dfa229a0786bca4d63d6d70f8b822f0
Daniel Oliveira <danieldfs18@hotmail.com>
2023-07-27 19:49:02 -0300

    fix distance calculation

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -54,12 +54,12 @@ useEffect(() => {
   function calculateDistance(lat1, lon1, lat2, lon2) {
     const earthRadiusKm = 6371;
   
-    const dLat = deg2rad(lat2 - lat1);
-    const dLon = deg2rad(lon2 - lon1);
+    const dLat = rad2deg(lat2 - lat1);
+    const dLon = rad2deg(lon2 - lon1);
   
     const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+      Math.sin(deg2rad(dLat) / 2) * Math.sin(deg2rad(dLat) / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(dLon) / 2) * Math.sin(deg2rad(dLon) / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   
     const distance = earthRadiusKm * c;
@@ -69,6 +69,11 @@ useEffect(() => {
   function deg2rad(deg) {
     return deg * (Math.PI / 180);
   }
+  
+  function rad2deg(rad) {
+    return rad * (180 / Math.PI);
+  }
+  
      
   function calculateDistanceBetweenCenterAndCurrentLocation() {
     if (center && currentLocation) {
