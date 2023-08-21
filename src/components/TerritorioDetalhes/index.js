11e79047dc114820a8d95996c9292c236c95e6b2
Daniel Oliveira <danieldfs18@hotmail.com>
2023-08-21 12:23:29 -0300

    add download button

--- src/components/TerritorioDetalhes/index.js
+++ src/components/TerritorioDetalhes/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './style.css';
-
+import html2pdf from 'html2pdf.js'; // Importe a biblioteca html2pdf
 
 function TerritorioDetalhes({ territory }) {
   const [showExtraContent, setShowExtraContent] = useState(false);
@@ -9,14 +9,30 @@ function TerritorioDetalhes({ territory }) {
     setShowExtraContent(!showExtraContent);
   };
 
+  const generatePDF = () => {
+    const contentElement = document.getElementById('territorio-content');
+    const opt = {
+      margin:       10,
+      filename:     'territorio.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+    
+    html2pdf().from(contentElement).set(opt).save();
+  };
+
   return (
     <div className="territorio-detalhes">
       <div className="territorio-info">
         <div className="territorio-header">
-          <h2>{territory.name}</h2>
+          <div className="header-content">
+            <h2>{territory.name}</h2>
+            <button className="button" onClick={generatePDF}>Baixar PDF</button>
+          </div>
           <img src={`data:image/jpeg;base64, ${territory.mainImage}`} alt={territory.name} className="capa" />
         </div>
-        <div className="territorio-text">
+        <div className="territorio-text" id="territorio-content"> {/* Added an ID */}
           <h3>Descrição</h3>
           <div dangerouslySetInnerHTML={{ __html: territory.briefDescription }}></div>
           <h3>História</h3>
@@ -39,7 +55,7 @@ function TerritorioDetalhes({ territory }) {
           <div dangerouslySetInnerHTML={{ __html: territory.religion }}></div>
           <h3>Referências</h3>
           <div dangerouslySetInnerHTML={{ __html: territory.reference }}></div>       
-          </div>
+        </div>
       </div>
     </div>
   );
